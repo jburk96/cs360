279c18a3849b00afd70b83fd9db5998988da0b42
jburk96 <jburk@eou.edu>
2018-05-30 10:31:08 -0700

    start up

---
+++ p4/BinaryTree.cpp
@@ -0,0 +1,54 @@
+BinaryTree()
+{
+
+}
+
+~BinaryTree()
+{
+
+}
+
+void addNode(int value)
+{
+
+}
+
+Node *search(int value)
+{
+
+}
+
+void cleanupTree()
+{
+
+}
+
+void printTree(bool ascending)
+{
+
+}
+
+void cleanupTree(Node *leaf)
+{
+
+}
+
+void printTreeAscending(Node *leaf)
+{
+
+}
+
+void printTreeDescending(Node *leaf)
+{
+
+}
+
+void addNode(int value, Node *leaf)
+{
+
+}
+
+Node *search(int value, Node *leaf)
+{
+
+}
---
+++ p4/Node.cpp
@@ -0,0 +1,34 @@
+Node(int val)
+{
+
+}
+
+~Node()
+{
+
+}
+
+int getValue()
+{
+
+}
+
+Node* getLeft()
+{
+
+}
+
+Node* getRight()
+{
+
+}
+
+void setLeft(Node* l)
+{
+
+}
+
+void setRight(Node* r)
+{
+
+}
